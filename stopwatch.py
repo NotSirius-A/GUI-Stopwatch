25ced7e59a612aaec00af645f96e6b009949f9a0
Not-SiriusA <filip.grzymski@gmail.com>
2021-02-24 12:32:36 +0100

    added basic timer functionality and organized a bit

--- stopwatch.py
+++ stopwatch.py
@@ -7,76 +7,129 @@ import tkinter as tk
 class App:
     def __init__(self, master):
         self.master = master
+        self.t_start_ns = 0
+        #time between updates in milisec
+        self.refresh_period_ms = 15
 
+        ##### Main frame setup #####
         main_frame = tk.Frame(master, bg='grey')
         main_frame.pack(expand=True, fill=tk.BOTH, side=tk.TOP)
 
+        ##### Stopwatch timer label setup #####
+        timer_style = {'padx':10, 'pady':10, 'bg':'red', 'font':('Calibri', 40)}
+        self.draw_timer(main_frame, timer_style)
 
-
-        self.timer_value = tk.StringVar()
-        self.timer_value.set(0)
-
-        font=('Calibri', 40)
-        timer_label = tk.Label(main_frame, width=7, height=1, padx=10, pady=10, textvariable=self.timer_value, bg='red', font=font)
-        timer_label.pack(pady=30, padx=70, fill=tk.BOTH, expand=True, side=tk.TOP)
-
-
-
-
+        ##### Middle frame setup (this is where buttons go) #####
         middle_frame = tk.Frame(main_frame, height=100, bg='purple')
         middle_frame.pack(padx=30, side=tk.TOP, fill=tk.BOTH, expand=True)
 
-
+        ##### Setting up buttons #####
         button_placement = {'padx':40, 'pady':35, 'side':tk.LEFT, 'fill':tk.BOTH, 'expand':True}
+        button_size = {'padx':25, 'pady':10}
+        self.draw_buttons(middle_frame, button_placement, button_size)
 
-        start_or_stop='Start'
-
-        StartStop_btn = tk.Button(middle_frame, padx=25, pady=10, bg='green', text=start_or_stop, command=StartStop_btn_func)
-        StartStop_btn.pack(button_placement)
-
-        reset_btn = tk.Button(middle_frame, padx=25, pady=10, bg='yellow', text='Reset', command=reset_btn_func)
-        reset_btn.pack(button_placement)
-
-        round_btn = tk.Button(middle_frame, padx=25, pady=10, bg='grey', text='Round', command=round_btn_func)
-        round_btn.pack(button_placement)
-
-
+        ##### bottom frame setup #####
         bottom_frame = tk.Frame(main_frame, height=200, bg='lightgreen')
         bottom_frame.pack(padx=30, side=tk.TOP, fill=tk.BOTH, expand=True)
 
+    def draw_timer(self, frame, timer_style):
+        #timer_style = {'padx':pixels, 'pady':pixels, 'bg':'color', 'font':('font', size)}
+
+        self.timer_value = tk.StringVar()
+        # '00:00:00' is the default value
+        self.timer_value.set('00:00:00.00')
+
+        self.timer_label = tk.Label(frame, timer_style, textvariable=self.timer_value)
+        self.timer_label.pack(pady=30, padx=70, fill=tk.BOTH, expand=True, side=tk.TOP)
+
+    def draw_buttons(self, frame, button_placement, button_size):
+        # button_placement = {'padx':pixels, 'pady':pixels, 'side':tk.LEFT, 'fill':tk.BOTH, 'expand':True}
+        # button_size = {'padx':pixels, 'pady':pixels}
 
 
+        self.StartStop_btn = tk.Button(frame, button_size, bg='green', text='Start', command=self.start_btn_func)
+        self.StartStop_btn.pack(button_placement)
+
+        self.reset_btn = tk.Button(frame, button_size, bg='yellow', text='Reset', command=self.reset_btn_func)
+        self.reset_btn.pack(button_placement)
+
+        self.round_btn = tk.Button(frame, button_size, bg='grey', text='Round', command=self.round_btn_func)
+        self.round_btn.pack(button_placement)
+
+    def start_btn_func(self):
+
+        #transfrom into stop button
+        self.StartStop_btn.config(bg='red', text='Stop', command=self.stop_btn_func)
+
+        #start counting here
+        self.t_start_ns = time.time_ns()
+
+        #the func below will start a timer update job
+        self.update_timer_value_job()
 
 
-    # x = time.time_ns()
-    # time.sleep(1)
-    # print(time_since_ns(x))
+        # print('start')
+        # print(self.master.winfo_height())
+        # print(self.master.winfo_width())
+
+    def update_timer_value_job(self):
+        #Start to update every x milliseconds when func is called
+        self.update_timer_job = self.master.after(self.refresh_period_ms, self.update_timer_value_job)
+
+        x = self.time_since_ns(self.t_start_ns)
+        self.timer_value.set(x/10e+8)
 
 
-############### DEF ####################
+    def stop_btn_func(self):
+        #stop updating
+        self.master.after_cancel(self.update_timer_job)
 
-def StartStop_btn_func():
-    print('startstop')
-    print(app.master.winfo_height())
-    print(app.master.winfo_width())
-
-    app.timer_value.set(10)
-
-def reset_btn_func():
-    print('reset')
+        #transform inro start button
+        self.StartStop_btn.config(bg='green', text='Start', command=self.start_btn_func)
 
 
-def round_btn_func():
-    print('round')
+    def reset_btn_func(self):
+        self.timer_value.set("0")
+        
+
+    def round_btn_func(self):
+        self.timer_value.set("1111s")
+        print('round')
 
 
-def time_since_ns(time_start_ns=0):
-    #this func return time in ns
-    #ns means nanoseconds
+    def time_since_ns(self, time_start_ns=0):
+        #this func return time in ns
+        #ns means nanoseconds
 
-    time_now = time.time_ns()
-    rv = time_now - time_start_ns
-    return rv
+        time_now = time.time_ns()
+        rv = time_now - time_start_ns
+        return rv
+
+
+#######################################
+
+
+# def StartStop_btn_func():
+#     global job1
+#     print('startstop')
+#     print(app.master.winfo_height())
+#     print(app.master.winfo_width())
+
+#     x = time_since_ns(start_time_ns)
+#     app.timer_value.set(x)
+
+#     job1 = app.master.after(500, StartStop_btn_func)
+
+
+# def reset_btn_func():
+#     print('reset')
+#     global job1
+#     app.master.after_cancel(job1)
+
+
+# def round_btn_func():
+#     print('round')
+
 
 
 if __name__ == '__main__':
